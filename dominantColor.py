a401089aa019e75bb2032dc2c1f0feb3bb46a995
Isabel Dominik <icd107@live.com>
2021-10-06 15:08:08 -0700

    Update dominantColor.py
    Shows color opposite on the color wheel of the dominant color. Need to debug in code

--- dominantColor.py
+++ dominantColor.py
@@ -31,7 +31,7 @@ def plot_colors2(hist, centroids):
 
 	for(percent, color) in zip(hist,centroids):
 		# plot the relative percentage of each cluster
-		endX = startX + (percent * 300)
+		endX = startX + (300)
 		cv2.rectangle(bar, (int(startX), 0), (int(endX), 50), color.astype("uint8").tolist(), -1)
 		startX = endX
 		break
@@ -48,11 +48,11 @@ while(True):
 	img = frame[200:400, 200:400]
 
 	if ret:
-		img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-		img = img.reshape((img.shape[0] * img.shape[1],3)) #represent as row*column,channel number
-		clt = KMeans(n_clusters = 3) #cluster number
-		clt.fit(img)
+		img2 = img2.reshape((img2.shape[0] * img2.shape[1],3)) #represent as row*column,channel number
+		clt = KMeans(n_clusters = 1) #cluster number
+		clt.fit(img2)
 
 		hist = find_histogram(clt)
 		bar = plot_colors2(hist, clt.cluster_centers_)
@@ -61,6 +61,7 @@ while(True):
 
 		cv2.imshow("stream", image)
 		cv2.imshow("majority color", bar)
+		cv2.imshow("rectange",img2)
 
 		#plt.axis("off")
 		#plt.imshow(bar)
