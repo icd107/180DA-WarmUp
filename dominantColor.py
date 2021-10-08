3328956969bee5e17b6faff8701d53df79d247f4
Isabel Dominik <icd107@live.com>
2021-10-07 21:07:03 -0700

    Update dominantColor.py

--- dominantColor.py
+++ dominantColor.py
@@ -31,10 +31,9 @@ def plot_colors2(hist, centroids):
 
 	for(percent, color) in zip(hist,centroids):
 		# plot the relative percentage of each cluster
-		endX = startX + (300)
+		endX = startX + (percent * 300)
 		cv2.rectangle(bar, (int(startX), 0), (int(endX), 50), color.astype("uint8").tolist(), -1)
 		startX = endX
-		break
 
 	# return the bar chart
 	return bar
@@ -51,7 +50,7 @@ while(True):
 		img2 = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 		img2 = img2.reshape((img2.shape[0] * img2.shape[1],3)) #represent as row*column,channel number
-		clt = KMeans(n_clusters = 1) #cluster number
+		clt = KMeans(n_clusters = 3) #cluster number
 		clt.fit(img2)
 
 		hist = find_histogram(clt)
@@ -61,11 +60,6 @@ while(True):
 
 		cv2.imshow("stream", image)
 		cv2.imshow("majority color", bar)
-		cv2.imshow("rectange",img2)
-
-		#plt.axis("off")
-		#plt.imshow(bar)
-		#plt.show()
 
 	# Quit if 'q' key pressed
 	if cv2.waitKey(1) & 0xFF == ord('q'):
