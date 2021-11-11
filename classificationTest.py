26ddb0ad9d4388faa356bc9ab25e3b6427222875
Isabel Dominik <icd107@live.com>
2021-11-10 16:04:14 -0800

    Update classificationTest.py

--- classificationTest.py
+++ classificationTest.py
@@ -10,43 +10,6 @@ import os
 import numpy as np
 import csv
 
-RAD_TO_DEG = 57.29578
-M_PI = 3.14159265358979323846
-G_GAIN = 0.070		  # [deg/s/LSB]  If you change the dps for gyro, you need to update this value accordingly
-AA =  0.40			  # Complementary filter constant
-MAG_LPF_FACTOR = 0.4	# Low pass filter constant magnetometer
-ACC_LPF_FACTOR = 0.4	# Low pass filter constant for accelerometer
-ACC_MEDIANTABLESIZE = 9		 # Median filter table size for accelerometer. Higher = smoother but a longer delay
-MAG_MEDIANTABLESIZE = 9		 # Median filter table size for magnetometer. Higher = smoother but a longer delay
-
-
-# Compass Calibration offset
-# Values found by running calibrateBerryIMU.py
-magXmin =  -22661
-magYmin =  32736
-magZmin =  3873
-magXmax =  -22584
-magYmax =  32767
-magZmax =  4260
-
-#Kalman filter variables
-Q_angle = 0.02
-Q_gyro = 0.0015
-R_angle = 0.005
-y_bias = 0.0
-x_bias = 0.0
-XP_00 = 0.0
-XP_01 = 0.0
-XP_10 = 0.0
-XP_11 = 0.0
-YP_00 = 0.0
-YP_01 = 0.0
-YP_10 = 0.0
-YP_11 = 0.0
-KFangleX = 0.0
-KFangleY = 0.0
-
-
 a = datetime.datetime.now()
 
 IMU.detectIMU()	 #Detect if BerryIMU is connected.
@@ -76,9 +39,11 @@ x_th_left = -2500
 y_th_front = 2000
 y_th_back = -1000
 
+movements = []
+
 t = 0
 # one minute: 1200
-while True:
+while t < 1200:
 
 	#Read the accelerometer,gyroscope and magnetometer values
 	ACCx = IMU.readACCx()
@@ -101,22 +66,29 @@ while True:
 	# Vertical classification
 	if ACCz > z_th_up:
 		print("Up!")
+		movements.append("U")
 	elif ACCz < z_th_down:
 		print("Down!")
+		movements.append("D")
 
 	# Left right classification
 	if ACCx > x_th_right:
 		print("Right!")
+		movements.append("R")
 	elif ACCx < x_th_left:
 		print("Left!")
+		movements.append("L")
 
 	# Front back classification
 	if ACCy > y_th_front:
 		print("Forward!")
+		movements.append("F")
 	elif ACCy < y_th_back:
 		print("Back!")
+		movements.append("B")
 	
 
 	#slow program down a bit, makes the output more readable
 	time.sleep(0.05)
 	t += 1
+print(movements)
